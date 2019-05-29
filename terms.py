7791a1ab725ca1cd3733f4e3471f7c7064306fa0
jonahtash <jonahntash@gmail.com>
2019-05-29 14:15:06 -0400

    Multi-subpiece normalization

--- terms.py
+++ terms.py
@@ -13,8 +13,11 @@ out_csv = 'term_table.csv'
 highest_level = 15
 
 # Create 2 tables, Originals and Terms
-cur.execute('CREATE TABLE Originals (terms text, max_level INTEGER)')
-cur.execute('CREATE TABLE Terms (piece text, normalized_piece text, orignal text, break_level INTEGER)')
+# 3 Coluns: Originals
+# 8 Columns: Terms
+cur.execute('CREATE TABLE Originals (terms text, max_level INTEGER, ID text)')
+cur.execute('CREATE TABLE Terms (piece text, alphabetical text, normalized_piece text, freq INTEGER, norm_freq INTEGER, break_level INTEGER, orignal text, ID text)')
+cur.execute('CREATE INDEX idx ON Terms (alphabetical, break_level)')
 
 # Populate Originals
 for row in csv.reader(open(in_csv, 'r')):
@@ -22,7 +25,7 @@ for row in csv.reader(open(in_csv, 'r')):
     if not delims:
         delims = [':0:']
     max_level = max(set(delims), key = lambda s: int(s[1:-1]))
-    cur.execute('INSERT INTO Originals VALUES (?, ?)', (row[0], int(max_level[1:-1])))
+    cur.execute('INSERT INTO Originals VALUES (?, ?, ?)', (row[0], int(max_level[1:-1]), row[3]))
 
 #Populate Terms
 for i in range(highest_level, 0, -1):
@@ -32,31 +35,35 @@ for i in range(highest_level, 0, -1):
         pieces = row[0].split(':'+ias+':')
         if len(pieces) > 1:
             for piece in pieces:
-                cur.execute('INSERT INTO Terms VALUES (?, ?, ?, ?)', (piece, piece, row[0], i))
+                alph_list = piece.split(':'+str(i-1)+':')
+                alph_list.sort()
+                alph = (':'+str(i-1)+':').join(alph_list)
+                cur.execute('INSERT INTO Terms VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (piece, alph, piece, 0, 0, i, row[0], row[2]))
     cur.execute('SELECT * FROM Terms WHERE break_level = '+str(i+1))
     for row in cur.fetchall():
         pieces = row[0].split(':'+ias+':')
         if len(pieces) > 1:
             for piece in pieces:
-                cur.execute('INSERT INTO Terms VALUES (?, ?, ?, ?)', (piece, piece, row[1], i))
+                alph_list = piece.split(':'+str(i-1)+':')
+                alph_list.sort()
+                alph = (':'+str(i-1)+':').join(alph_list)
+                cur.execute('INSERT INTO Terms VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (piece, alph, piece, 0, 0, i, row[6], row[7]))
 # Normalize Data
 for i in range(1, highest_level+1, 1):
     ias = str(i)
     cur.execute('SELECT DISTINCT piece FROM Terms WHERE break_level = '+ias)
     for row in cur.fetchall():
-        roots = row[0].split(':'+str(i-1)+':')
-        if len(roots) > 1:
-            b_a = roots[1]+':'+str(i-1)+':'+roots[0]
-            count_a_b = int(cur.execute('SELECT COUNT(*) FROM Terms WHERE piece = ? AND break_level = ?', (row[0], i)).fetchall()[0][0])
-            count_b_a = int(cur.execute('SELECT COUNT(*) FROM Terms WHERE piece = ? AND break_level = ?', (b_a, i)).fetchall()[0][0])
-            n_phrase = row[0]
-            if count_b_a > count_a_b:
-                n_phrase = roots[1]+':'+str(i-1)+':'+roots[0]
-                print(row[0], n_phrase)
-                cur.execute('UPDATE Terms SET normalized_piece = ? WHERE piece = ? AND break_level = ?', (n_phrase, row[0], i))
-                cur.execute('UPDATE Terms SET piece = REPLACE(piece, ?, ?),\
-                        normalized_piece = REPLACE(normalized_piece, ?, ?) WHERE break_level > ?', (row[0], n_phrase, row[0], n_phrase, i))
+        freq = int(cur.execute('SELECT COUNT(*) FROM Terms WHERE piece = ? AND break_level = ?', (row[0], i)).fetchall()[0][0])
+        cur.execute('UPDATE Terms SET freq = ? WHERE piece = ? AND break_level = ?', (freq, row[0], i))
 
+    cur.execute('SELECT DISTINCT alphabetical from Terms WHERE break_level = '+ias)
+    for row in cur.fetchall():
+        cur.execute('SELECT piece, freq FROM Terms WHERE alphabetical = ? AND break_level = ? ORDER BY freq DESC', (row[0], i))
+        nr = cur.fetchone()
+        cur.execute('UPDATE Terms SET normalized_piece = ?, norm_freq = ? WHERE alphabetical = ? AND break_level = ?', (nr[0], nr[1], row[0], i))
+        for old_row in cur.fetchall():
+            cur.execute('UPDATE Terms SET piece = REPLACE(piece, ?, ?),\
+                normalized_piece = REPLACE(normalized_piece, ?, ?) WHERE break_level > ?', (old_row[0], nr[0], old_row[0], nr[0], i))
 
 # Output to csv
-pd.read_sql(sql='SELECT * FROM terms ORDER BY break_level DESC', con=conn).to_csv(out_csv, index=False, sep=',', quoting=csv.QUOTE_NONNUMERIC, encoding='utf-8-sig')
+pd.read_sql(sql='SELECT * FROM terms ORDER BY orignal DESC, break_level ASC', con=conn).to_csv(out_csv, index=False, sep=',', quoting=csv.QUOTE_NONNUMERIC, encoding='utf-8-sig')
